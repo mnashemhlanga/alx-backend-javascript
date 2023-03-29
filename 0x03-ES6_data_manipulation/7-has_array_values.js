9f5a65ef61249b5d06e3296ba3acbbc4e8094635
munashe mhlanga <mnashemhlanga8@gmail.com>
2023-03-29 04:58:53 +0200

    second half solutions

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,13 @@
+/* eslint-disable array-callback-return */
+export default function hasValuesFromArray(set, list) {
+  let verdict = true;
+  list.map((x) => {
+    if (!set.has(x)) {
+      verdict = false;
+    }
+  });
+
+  return verdict;
+}
+
+
---
+++ 0x03-ES6_data_manipulation/9-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import groceriesList from './9-groceries_list.js';
+
+console.log(groceriesList());
+
